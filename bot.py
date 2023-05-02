ed9ca994f9e6c103e8015449b13c90a8bbdec3de
RustyRin <ggk494@gmail.com>
2023-05-02 13:48:57 -0400

    the bot lives

---
+++ bot.py
@@ -0,0 +1,30 @@
+"""
+OwOverwatch Botty
+
+Python Discord Bot for my friends Overwatch (mostly) server
+
+"""
+
+import discord
+from discord.ext import commands, tasks
+import discord.utils
+
+with open("./database/secrets/discord.txt") as f:
+    DISCORD_TOKEN = str(f.readline())
+
+bot_prefix = commands.when_mentioned_or("!ow")
+bot_intents = discord.Intents.default()
+discord_client = commands.Bot(
+    command_prefix=bot_prefix,
+    help_command=None,
+    intents=bot_intents
+)
+
+
+@discord_client.event
+async def on_ready():
+    print("Bot running!")
+    await discord_client.change_presence(status=discord.Status.online, activity=discord.Game("@me"))
+
+
+discord_client.run(token=DISCORD_TOKEN)
